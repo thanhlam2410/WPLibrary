d1671f15735fc86c13c84bf13a6ddd775fec8ea4
Kana <Kana@DESKTOP-F384QDO>
2016-08-23 10:02:05 +0700

    Implement BindingBase class

---
+++ src/WPCore/WPCore.Shared/Binding/BindingBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace WPCore.Binding
+{
+    public class BindingBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void Set<T>(ref T target, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (!object.Equals(target, value))
+            {
+                target = value;
+                OnPropertyChanged(propertyName);
+            }
+        }
+
+        /// <summary>
+        /// Notifies listeners that a property value has changed.
+        /// </summary>
+        /// <param name="propertyName">Name of the property used to notify listeners.  This
+        /// value is optional and can be provided automatically when invoked from compilers
+        /// that support <see cref="CallerMemberNameAttribute"/>.</param>
+        protected void OnPropertyChanged(string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
